4ffc9e72da1d278b7040e03d59652e607bf89d86
unknown <nsteinm2@gmail.com>
2018-06-01 00:17:34 -0400

    upload portfolio site

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 
+$(document).ready(function(){
+  $("#portOverlayOne").click(function(){
+    if($(this).css('opacity') == 0){
+      $(this).animate({opacity:1}, 1000);
+    } else {
+      $(this).animate({opacity:0}, 1000);
+    }
+  });
+});
